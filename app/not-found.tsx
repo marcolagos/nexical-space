98ab5abac5b0ec96db5ff7487e72d0fbcc66fa3a
Marco Lagos <marcolagos888@gmail.com>
2023-10-08 16:34:02 -0500

    Added Not Found Page

---
+++ app/not-found.tsx
@@ -0,0 +1,27 @@
+import Link from '@/components/Link'
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
+      <div className="space-x-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
+          404
+        </h1>
+      </div>
+      <div className="max-w-md">
+        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
+          This page does not exist.
+        </p>
+        <p className="mb-8">
+          However, there are still other profound elements in the nexical space.
+        </p>
+        <Link
+          href="/"
+          className="focus:shadow-outline-blue inline rounded-lg border border-transparent px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors bg-gray-900 duration-150 hover:bg-orange-100 focus:outline-none dark:hover:bg-gray-800"
+        >
+          Back to the nexical space
+        </Link>
+      </div>
+    </div>
+  )
+}
