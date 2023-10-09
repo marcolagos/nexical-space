e770aaf92eddfa720117c5c4bf08d0fb8991b264
Marco Lagos <marcolagos888@gmail.com>
2023-10-09 17:03:21 -0500

    Added Sitemap XML and More Example Data

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+import { allReferences } from 'contentlayer/generated'
+import siteMetadata from '@/data/siteMetadata'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = siteMetadata.siteUrl
+  const referenceRoutes = allReferences.map((post) => ({
+    url: `${siteUrl}/${post.path}`,
+    lastModified: post.lastmod || post.date,
+  }))
+
+  const routes = ['', 'references', 'projects', 'tags'].map((route) => ({
+    url: `${siteUrl}/${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes, ...referenceRoutes]
+}
