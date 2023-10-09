0b7a82d1e03b832386edd6e207d9af87dc6c5326
Marco Lagos <marcolagos888@gmail.com>
2023-10-09 16:52:45 -0500

    Added Example data

---
+++ data/projectsData.ts
@@ -0,0 +1,22 @@
+const projectsData = [
+  {
+    title: 'A Search Engine',
+    description: `What if you could look up any information in the world? Webpages, images, videos
+      and more. Google has many features to help you find exactly what you're looking
+      for.`,
+    imgSrc: '',
+    href: 'https://www.google.com',
+    github: 'link',
+    tech: ['Next.js', 'Tailwind CSS', 'TypeScript'],
+  },
+  {
+    title: 'Example project',
+    description: `This is some description`,
+    imgSrc: '',
+    href: '/references/code-sample',
+    github: 'link',
+    tech: ['Next.js', 'Tailwind CSS', 'TypeScript'],
+  },
+]
+
+export default projectsData
