2eec35abbb553d78623340b31cfceab2b35c1a09
Marco Lagos <marcolagos888@gmail.com>
2023-10-09 17:02:23 -0500

    Created Main Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <main>Hello World</main>
+import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
+import { allReferences } from 'contentlayer/generated'
+import Main from './Main'
+
+export default async function Page() {
+  const sortedPosts = sortPosts(allReferences)
+  const posts = allCoreContent(sortedPosts)
+  return <Main posts={posts} />
 }
